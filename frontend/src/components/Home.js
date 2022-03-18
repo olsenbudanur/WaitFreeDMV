f0bfbaaa5b75f66fe78ed24ce5f583932a6d4448
Meng Fan Wang <mengfanwang724@gmail.com>
2022-03-18 18:01:08 -0400

    updated logo

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,6 +5,7 @@ import sloth from './assets/sloth.gif';
 import "./Styles.css";
 import Button from '@mui/material/Button';
 import { Animator, batch, ScrollContainer, ScrollPage, Sticky, Fade, MoveOut, ZoomIn, StickyIn, FadeIn } from 'react-scroll-motion'
+import Typewriter from 'typewriter-effect';
 
 const ZoomInScrollsOut = batch(StickyIn(), FadeIn(),ZoomIn() );
 const Home = () => {
@@ -15,7 +16,13 @@ const Home = () => {
           <ScrollPage page = {0}>
           <Parallax className="parallax" bgImage={sloth} bgImageAlt="slow dmv sloth from utopia" strength={500}>
           <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
-             <div className="title">Wait Free DMV</div>
+            <div className="title">
+            <Typewriter 
+              onInit = {(typewriter => {
+                typewriter.typeString("Tired  of waiting?").pauseFor(1500).deleteAll().typeString("Wait Free DMV").start();
+              })}
+            /></div>
+             {/* <div className="title">Wait Free DMV</div> */}
           </Animator>
           </Parallax>
           <div className="home-text">
