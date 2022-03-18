a1ddae381e94c159cf376ff45e6647fb8a615083
Meng Fan Wang <mengfanwang724@gmail.com>
2022-03-18 17:11:42 -0400

    moved scrolling to home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,7 @@ import Navbar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Appointment from './components/Appointment';
-import { Animator, batch, ScrollContainer, ScrollPage, Sticky, Fade, MoveOut, ZoomIn, StickyIn, FadeIn } from 'react-scroll-motion'
 
-const ZoomInScrollsOut = batch(StickyIn(), FadeIn(),ZoomIn() );
 function App() {
   return (
     <>
@@ -18,19 +16,6 @@ function App() {
         <Route path="/appointment" element={<Appointment />} />
       </Routes>
     </Router>
-
-    <ScrollContainer>
-      <ScrollPage page = {0}>
-        <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
-          <h2>WAIT FREE DMV</h2>
-        </Animator>
-      </ScrollPage>
-      <ScrollPage page = {1}>
-        <Animator animation={ZoomInScrollsOut}>
-          <h2>Why Wait?</h2>
-        </Animator>
-      </ScrollPage>
-    </ScrollContainer>
     </>
 
   );
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,27 +4,46 @@ import { Parallax } from 'react-parallax';
 import sloth from './assets/sloth.gif';
 import "./Styles.css";
 import Button from '@mui/material/Button';
+import { Animator, batch, ScrollContainer, ScrollPage, Sticky, Fade, MoveOut, ZoomIn, StickyIn, FadeIn } from 'react-scroll-motion'
 
+const ZoomInScrollsOut = batch(StickyIn(), FadeIn(),ZoomIn() );
 const Home = () => {
   return (
+    
     <div>
-        <Parallax className="parallax" bgImage={sloth} bgImageAlt="slow dmv sloth from utopia" strength={500}>
-          <div className="title">Wait Free DMV</div>
-        </Parallax>
-        
-        <div className="home-text">
+        <ScrollContainer>
+          <ScrollPage page = {0}>
+          <Parallax className="parallax" bgImage={sloth} bgImageAlt="slow dmv sloth from utopia" strength={500}>
+          <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+             <div className="title">Wait Free DMV</div>
+          </Animator>
+          </Parallax>
+          <div className="home-text">
           <b> Why wait for an DMV appointment? Schedule now and save time!</b>
-        </div>
-        <div className="home-text">
+          </div>
+          <div className="home-text">
           <Button variant="contained" href="/appointment"
              size='large'>
-            One-Click Schedule</Button>
-        </div>
+            One-Click Schedule
+          </Button>
+          </div>
+          </ScrollPage>
+
+          <ScrollPage page = {1}>
+            <Animator animation={ZoomInScrollsOut}>
+              <h2>What is Wait Free DMV?</h2>
+            </Animator>
+          </ScrollPage>
+        </ScrollContainer>
+
+        {/* <Parallax className="parallax" bgImage={sloth} bgImageAlt="slow dmv sloth from utopia" strength={500}>
+          <div className="title">Wait Free DMV</div>
+        </Parallax>
 
         <div className='home-text'>
           <b>What is Wait Free DMV?</b>
 
-        </div>
+        </div> */}
       
       </div>
   );
