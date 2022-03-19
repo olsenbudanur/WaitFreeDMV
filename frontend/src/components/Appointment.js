c4ae38b61d5620296526ec492cbaf1711997bab8
olsenbudanur <74462406+olsenbudanur@users.noreply.github.com>
2022-03-18 20:53:09 -0400

    Update Appointment.js

--- frontend/src/components/Appointment.js
+++ frontend/src/components/Appointment.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from 'react';  
+import db from "./firebase_config";
+import { doc, setDoc } from "firebase/firestore";
 import { Button, TextField, FormControl, InputLabel, Select, MenuItem, DialogContent} from '@mui/material';
 import "./Styles.css";
 
 const Appointment = () => {
-
   const [state, setState] = React.useState('');
   const [distance, setDistance] = React.useState('');
   const [type, setType] = React.useState('');
+  
+  const [number, setNumber] = React.useState('');
+  const [time, setTime] = React.useState('');
+  const [zip, setZip] = React.useState('');
+
+
   var types = ["Driver and ID Card Services", "Learner's Permit / Knowledge Test", "Road Skills Testing", "Motorcyle Skills Testing", "Hazmat Fingerprint / Background Check", "E-ZPass"];
   
   const handleChangeState = (event) => {
@@ -19,12 +26,38 @@ const Appointment = () => {
     setType(event.target.value);
   };
 
+
   var typeList = types.map(function(type){
-    return <MenuItem value={type} key="{type}">{type}</MenuItem>;
+    return <MenuItem value={type}>{type}</MenuItem>;
   })
 
 
   const states = ['VA', 'MD', 'NC', 'DC']
+  const pushUserRequest = async(e) => {
+    console.log(state);
+    console.log(type);
+
+    console.log(time);
+    console.log(zip);
+    console.log(number);
+    //
+    // Add it to the database.
+    
+    let timeLimit = Date.now() + (time * 7 * 86400000);
+
+    await setDoc(doc(db, "UserData", number), {
+      state: state,
+      type: type,
+      zip: zip,
+      number: number,
+      timeLimit: timeLimit
+    });
+
+    //
+    // Alert the user.
+    alert("We will let you know if there are any available appointments!")
+    window.location.reload(false);
+  }
 
   return (
     <div>
@@ -35,7 +68,7 @@ const Appointment = () => {
         <div className='column'>
         
         <div style={{padding:'10px'}}>
-        <FormControl sx={{ m: 1, minWidth: '25%' }} >
+        <FormControl sx={{ m: 1, minWidth: '25%' }}>
         <InputLabel id="demo-simple-select-label">State</InputLabel>
         <Select
           labelId="demo-simple-select-label"
@@ -53,7 +86,8 @@ const Appointment = () => {
         </div>
 
         <div style={{padding:'10px'}}>
-        <TextField id="standard-basic" label="ZIP Code" type="number" variant="standard" />
+        <TextField id="standard-basic" label="ZIP Code" type="number" variant="standard" 
+          value={zip} onChange= {(e) => setZip(e.target.value)}/>
         </div>
 
         <div style={{padding:'10px'}}>
@@ -90,14 +124,16 @@ const Appointment = () => {
         </div>
 
         <div style={{padding:'10px'}}>
-        <TextField id="standard-basic" label="Number" required type="number" variant="standard" />
+        <TextField id="standard-basic" label="Number" type="number" variant="standard"
+          value={number} onChange= {(e) => setNumber(e.target.value)}/>
         </div>
 
         <div style={{padding:'10px'}}>
-        <TextField id="standard-basic" label="Dealine(# of weeks from now)" required type="number" variant="standard" />
+        <TextField id="standard-basic" label="Dealine(# of weeks from now)" type="number" variant="standard"
+          value={time} onChange= {(e) => setTime(e.target.value)}/>
         </div>
 
-        <div className='text'><Button variant="contained" id="apptbtn">Submit</Button></div>
+        <div className='text'><Button variant="contained" id="apptbtn" onClick={pushUserRequest}>Submit</Button></div>
         
         </div>
         <div className='column'>
