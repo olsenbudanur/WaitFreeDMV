da2a47580596db2962307bae4497e9b315c748d3
Niya Ma <78030299+Bellatrixmny@users.noreply.github.com>
2022-03-19 01:03:09 -0400

    final of friday

--- frontend/src/components/Appointment.js
+++ frontend/src/components/Appointment.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import db from "./firebase_config";
 import { doc, setDoc } from "firebase/firestore";
-import { Button, TextField, FormControl, InputLabel, Box, Select, MenuItem, DialogContent} from '@mui/material';
+import { Button, TextField, FormControl, InputLabel, Box, 
+        Select, MenuItem, DialogContent} from '@mui/material';
 import "./Styles.css";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import MapSection from './Map'
@@ -22,7 +23,12 @@ const Appointment = () => {
   const [zip, setZip] = React.useState('');
 
 
-  var types = ["Driver and ID Card Services", "Learner's Permit / Knowledge Test", "Road Skills Testing", "Motorcyle Skills Testing", "Hazmat Fingerprint / Background Check", "E-ZPass"];
+  var types = ["Driver and ID Card Services", 
+              "Learner's Permit / Knowledge Test", 
+              "Road Skills Testing", 
+              "Motorcyle Skills Testing", 
+              "Hazmat Fingerprint / Background Check", 
+              "E-ZPass"];
 
   const handleChangeState = (event) => {
     setState(event.target.value);
@@ -69,99 +75,127 @@ const Appointment = () => {
 
   return (
     <div>
-      <div className="text"><h1>Make Appointment</h1></div>
+      <div className="text">
+        <h1>Make Appointment</h1>
+      </div>
 
       <div className='row'>
 
         <div className='column'>
 
-        <div style={{padding:'10px'}}>
-        <FormControl sx={{ m: 1, minWidth: '30%' }}>
-        <InputLabel id="demo-simple-select-label">State</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={state}
-          label='State'
-          onChange={handleChangeState}
-        >
-          <MenuItem value={'DC'}>DC</MenuItem>
-          <MenuItem value={'MD'}>MD</MenuItem>
-          <MenuItem value={'VA'}>VA</MenuItem>
-          <MenuItem value={'NC'}>NC</MenuItem>
-        </Select>
-        </FormControl>
+          <div style={{padding:'10px'}}>
+            <FormControl sx={{ m: 1, minWidth: '30%' }}>
+              <InputLabel id="demo-simple-select-label">State</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={state}
+                  label='State'
+                  onChange={handleChangeState}
+                  >
+                  <MenuItem value={'DC'}>DC</MenuItem>
+                  <MenuItem value={'MD'}>MD</MenuItem>
+                  <MenuItem value={'VA'}>VA</MenuItem>
+                  <MenuItem value={'NC'}>NC</MenuItem>
+                </Select>
+            </FormControl>
+          </div>
+
+          <div style={{padding:'10px'}}>
+            <FormControl sx={{ m: 1, minWidth: '30%' }}>
+              <TextField id="standard-basic" 
+                label="ZIP Code" 
+                type="number" 
+                variant="outlined"
+                value={zip} 
+                onChange= {(e) => setZip(e.target.value)}/>
+            </FormControl>
+          </div>
+
+          <div style={{padding:'10px'}}>
+            <FormControl sx={{ m: 1, minWidth: '30%' }}>
+              <InputLabel id="demo-simple-select-label">
+                Distance(miles)
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={distance}
+                label='State'
+                onChange={handleChangeDistance}
+              >
+                <MenuItem value={'5'}>5</MenuItem>
+                <MenuItem value={'10'}>10</MenuItem>
+                <MenuItem value={'15'}>15</MenuItem>
+                <MenuItem value={'30'}>30</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+
+          <div className='text'>
+            <Button variant="contained" id="apptbtn">
+              Filter By Location</Button>
+          </div>
+
+          <div style={{padding:'10px'}}>
+            <FormControl sx={{ m: 1, minWidth: '30%' }}>
+              <InputLabel id="demo-simple-select-label">
+                Type of Appointment
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={type}
+                label='type'
+                onChange={handleChangeType}
+              >
+                {typeList}
+              </Select>
+            </FormControl>
+          </div>
+
+          <div style={{padding:'10px'}}>
+            <FormControl sx={{ m: 1, minWidth: '30%' }}>
+              <TextField 
+                id="standard-basic" 
+                label="Number" 
+                type="number" 
+                variant="outlined"
+                value={number} 
+                onChange= {(e) => setNumber(e.target.value)}/>
+            </FormControl>
+          </div>
+
+          <div style={{padding:'10px'}}>
+            <FormControl sx={{ m: 1, minWidth: '30%' }}>
+              <TextField 
+                id="standard-basic" 
+                label="Deadline(# of weeks from now)" 
+                type="number" 
+                variant="outlined"
+                value={time} 
+                onChange= {(e) => setTime(e.target.value)}/>
+            </FormControl>
+          </div>
+
+          <div className='text'>
+            <Button 
+              variant="contained" 
+              id="apptbtn" 
+              onClick={pushUserRequest}
+            >
+                Submit
+            </Button></div>
         </div>
 
-        <div style={{padding:'10px'}}>
-        <FormControl sx={{ m: 1, minWidth: '30%' }}>
-        <TextField id="standard-basic" label="ZIP Code" type="number" variant="outlined"
-          value={zip} onChange= {(e) => setZip(e.target.value)}/>
-        </FormControl>
-        </div>
 
-        <div style={{padding:'10px'}}>
-        <FormControl sx={{ m: 1, minWidth: '30%' }}>
-        <InputLabel id="demo-simple-select-label">Distance(miles)</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={distance}
-          label='State'
-          onChange={handleChangeDistance}
-        >
-          <MenuItem value={'5'}>5</MenuItem>
-          <MenuItem value={'10'}>10</MenuItem>
-          <MenuItem value={'15'}>15</MenuItem>
-          <MenuItem value={'30'}>30</MenuItem>
-        </Select>
-        </FormControl>
-        <div className='text'><Button variant="contained" id="apptbtn">Filter By Location</Button></div>
-        </div>
-
-        <div style={{padding:'10px'}}>
-          <FormControl sx={{ m: 1, minWidth: '30%' }}>
-          <InputLabel id="demo-simple-select-label">Type of Appointment</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={type}
-            label='type'
-            onChange={handleChangeType}
-          >
-            {typeList}
-          </Select>
-          </FormControl>
-        </div>
-
-        <div style={{padding:'10px'}}>
-          <FormControl sx={{ m: 1, minWidth: '30%' }}>
-          <TextField id="standard-basic" label="Number" type="number" variant="outlined"
-            value={number} onChange= {(e) => setNumber(e.target.value)}/>
-          </FormControl>
-        </div>
-
-        <div style={{padding:'10px'}}>
-        <FormControl sx={{ m: 1, minWidth: '30%' }}>
-        <TextField id="standard-basic" label="Deadline(# of weeks from now)" type="number" variant="outlined"
-          value={time} onChange= {(e) => setTime(e.target.value)}/>
-        </FormControl>
-        </div>
-
-        <div className='text'><Button variant="contained" id="apptbtn" onClick={pushUserRequest}>Submit</Button></div>
-
-        </div>
         <div className='column'>
-
-         <div>
-         <MapSection location={location} zoomLevel={13} /> {/* include it here */}
+          <div>
+            <MapSection location={location} zoomLevel={13} />
+          </div>
         </div>
-        </div>
-
-
 
       </div>
-
 
     </div>
   );
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { Parallax } from 'react-parallax';
 import sloth from './assets/sloth.gif';
 import "./Styles.css";
@@ -7,56 +6,73 @@ import Button from '@mui/material/Button';
 import { Animator, batch, ScrollContainer, ScrollPage, Sticky, Fade, MoveOut, ZoomIn, StickyIn, FadeIn } from 'react-scroll-motion'
 import Typewriter from 'typewriter-effect';
 
+
 const ZoomInScrollsOut = batch(StickyIn(), FadeIn(),ZoomIn() );
+
 const Home = () => {
   const [state, setState] = useState('');
     
   const handler = (event) => {
-      // changing the state to the name of the key
+    // changing the state to the name of the key
     // which is pressed
     setState(true);
   };
   return (
     <div>
         <ScrollContainer>
+
           <ScrollPage page = {0}>
-          <Parallax className="parallax" bgImage={sloth} bgImageAlt="slow dmv sloth from utopia" strength={500}>
-          <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
-            <div className="title">
-            <Typewriter 
-              onInit = {(typewriter => {
-                typewriter.typeString("Tired of waiting?").pauseFor(1500).deleteAll().typeString("WaitFree DMV").start();
-              })}
-            /></div>
-             {/* <div className="title">Wait Free DMV</div> */}
-          </Animator>
-          </Parallax>
-          <div className="home-text">
-          <b> Why wait for an DMV appointment? Schedule now and save time!</b>
-          </div>
-          <div className="home-text">
-          <Button variant="contained" href="/appointment" id ="apptbtn"
-             size='large' style={{zIndex:'10'}}>
-            One-Click Schedule
-          </Button>
-          </div>
+            <Parallax 
+              className="parallax" 
+              bgImage={sloth} 
+              bgImageAlt="slow dmv sloth from zootopia" 
+              strength={500}>
+
+              <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+                <div className="title">
+                  <Typewriter 
+                    onInit = {(typewriter => {
+                      typewriter.
+                      typeString("Tired of waiting?").
+                      pauseFor(1500).
+                      deleteAll().
+                      typeString("WaitFree DMV!").
+                      start();
+                    })}
+                  />
+                </div>
+              </Animator>
+            </Parallax>
+            <div className="home-text" style={{paddingTop:'20px'}}>
+              <b>Save time and alert me when an appointment is available!</b>
+            </div>
+              <div className="home-text" >
+                <Button variant="contained" href="/appointment" id ="apptbtn"
+                  style={{zIndex:'10', width:'500px', 
+                        height:'120px', fontSize:'40px'}}>
+                  Get text alert
+                </Button>
+              </div>
           </ScrollPage>
 
           <ScrollPage page = {1} style={{zIndex:'1'}}>
             <Animator animation={ZoomInScrollsOut}>
               <h2 className="h2txt">What is WaitFree DMV?</h2>
             </Animator>
-            {/* <p className="center">
-              Press any keys to continue
-            </p> */}
-            <div className="center"><Typewriter 
-              onInit = {(typewriter => {
-                typewriter.typeString("WaitFreeDMV allows you to save time scrolling through DMV webpage and book your appointment faster!").pauseFor(5000).start();
-              })}
-            /></div>
+            <div className="center" style={{position: 'absolute', bottom: 300}}>
+              What is WaitFree DMV!?
+              Have you ever had to get a DMV appointment ASAP, 
+              only to realize the soonest time slot is 3 months away? 
+              Well, WaitFree DMV! is here to help you out! 
+              Just send us the latest time you’d like your appointment, 
+              and your phone number. 
+              We will constantly scrape your DMV’s website for you, 
+              and send you an SMS reminder if any appointments become available 
+              within your desired time frame!
+            </div>
           </ScrollPage>
-        </ScrollContainer>
 
+        </ScrollContainer>
       
       </div>
   );
