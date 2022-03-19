21122ab5ec28a3c330af5abbc064749252b884bd
olsenbudanur <74462406+olsenbudanur@users.noreply.github.com>
2022-03-18 21:14:03 -0400

    Update dmvscraper.py

--- backend/dmvscraper.py
+++ backend/dmvscraper.py
@@ -30,129 +30,131 @@ db = firestore.client()
 
 #
 # Set up the credentials for Twilio
-account_sid = 'CANT SHARE THIS HERE'
-auth_token = 'CANT SHARE THIS HERE'
-twilio_number = 'CANT SHARE THIS HERE'
+account_sid = 'SECRET'
+auth_token = 'SECRET'
+twilio_number = 'SECRET'
 client = Client(account_sid, auth_token)
 
 
-#
-# Get the user data collection.
-doc_ref = db.collection(u'UserData')
-doc2 = doc_ref.get()
+while True:
+    #
+    # Get the user data collection.
+    doc_ref = db.collection(u'UserData')
+    doc2 = doc_ref.get()
 
-#
-# Loop through the user data, and add it to our list.
-userData = []
-for doc in doc2:
-    userData.append(doc)
-    print(f'Document data: {doc.to_dict()}')
-
-
-#
-# Uncomment this if you'd like to run it on COOL MODE!
-driver = webdriver.Chrome(PATH)
-
-#
-# These are the required options if you'd like to run the chrome headless.
-# Not as cool, but more efficent...
-# user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
-
-# options = webdriver.ChromeOptions()
-# options.headless = True
-# options.add_argument(f'user-agent={user_agent}')
-# options.add_argument("--window-size=1920,1080")
-# options.add_argument('--ignore-certificate-errors')
-# options.add_argument('--allow-running-insecure-content')
-# options.add_argument("--disable-extensions")
-# options.add_argument("--proxy-server='direct://'")
-# options.add_argument("--proxy-bypass-list=*")
-# options.add_argument("--start-maximized")
-# options.add_argument('--disable-gpu')
-# options.add_argument('--disable-dev-shm-usage')
-# options.add_argument('--no-sandbox')
-
-# driver = webdriver.Chrome(PATH, options=options);
+    #
+    # Loop through the user data, and add it to our list.
+    userData = []
+    for doc in doc2:
+        userData.append(doc)
+        print(f'Document data: {doc.to_dict()}')
 
 
+    #
+    # Uncomment this if you'd like to run it on COOL MODE!
+    driver = webdriver.Chrome(PATH)
 
-#
-# This chunk deals with setting up the scraping process.
-vars = {}
-driver.get("https://www.dmv.virginia.gov/onlineservices/appointments.aspx")
-driver.set_window_size(1440, 875)
-vars["window_handles"] = driver.window_handles
+    #
+    # These are the required options if you'd like to run the chrome headless.
+    # Not as cool, but more efficent...
+    # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
 
+    # options = webdriver.ChromeOptions()
+    # options.headless = True
+    # options.add_argument(f'user-agent={user_agent}')
+    # options.add_argument("--window-size=1920,1080")
+    # options.add_argument('--ignore-certificate-errors')
+    # options.add_argument('--allow-running-insecure-content')
+    # options.add_argument("--disable-extensions")
+    # options.add_argument("--proxy-server='direct://'")
+    # options.add_argument("--proxy-bypass-list=*")
+    # options.add_argument("--start-maximized")
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--no-sandbox')
 
-#
-# Click the "Schedule" button
-driver.find_element(By.CSS_SELECTOR, ".btn").click()
-
-
-#
-# Deal with switching windows on the chrome driver.
-time.sleep(round(2000 / 1000))
-wh_now = driver.window_handles
-wh_then = vars["window_handles"]
-if len(wh_now) > len(wh_then):
-    vars["win7946"] = set(wh_now).difference(set(wh_then)).pop()
-driver.switch_to.window(vars["win7946"])
-
-
-#
-# Click on "Other services" button
-time.sleep(round(2000 / 1000))
-driver.find_element(By.CSS_SELECTOR, ".QflowObjectItem:nth-child(7)").click()
-
-#
-# Click on new drivers license button.
-time.sleep(round(2000 / 1000))
-driver.find_element(By.CSS_SELECTOR, ".QflowObjectItem:nth-child(4)").click()
+    # driver = webdriver.Chrome(PATH, options=options);
 
 
-#
-# Click on the Arlington office.
-time.sleep(round(2000 / 1000))
-driver.find_element(By.CSS_SELECTOR, ".QflowObjectItem:nth-child(3)").click()
+    #
+    # This chunk deals with setting up the scraping process.
+    vars = {}
+    driver.get("https://www.dmv.virginia.gov/onlineservices/appointments.aspx")
+    driver.set_window_size(1440, 875)
+    vars["window_handles"] = driver.window_handles
 
 
-#
-# Get the next available appointment.
-time.sleep(round(2000 / 1000))
-el = driver.find_element(By.CSS_SELECTOR, ".DateTimeGrid-Day:nth-child(1)")
-
-#
-# Convert the next available appointment to epoch
-str = el.text.strip()
-my_date = datetime.strptime(str, "%m/%d/%Y")
-bestTimeEpoch = my_date.timestamp() * 1000
-
-print("The soonest time is: ", el.text)
-
-#
-# Get the users time restraint, let them know if the next available appointment 
-# is within the restraint.
-user = userData[0]
-userEpoch = user.get("timeLimit")
-userNumber = "+" + user.get("number")
+    #
+    # Click the "Schedule" button
+    driver.find_element(By.CSS_SELECTOR, ".btn").click()
 
 
-#
-# Send a message to the user
-message = "Hurry up, there is a DMV appointment available at " + str + "!!!"
-
-print(userEpoch)
-print(bestTimeEpoch)
-
-if bestTimeEpoch <= userEpoch:
-    send_to = '+12408104360'
-    message = client.messages.create(
-        body = message,
-        from_ = twilio_number,
-        to = userNumber
-    )
+    #
+    # Deal with switching windows on the chrome driver.
+    time.sleep(round(2000 / 1000))
+    wh_now = driver.window_handles
+    wh_then = vars["window_handles"]
+    if len(wh_now) > len(wh_then):
+        vars["win7946"] = set(wh_now).difference(set(wh_then)).pop()
+    driver.switch_to.window(vars["win7946"])
 
 
-#
-# Close the current window.
-driver.quit()
+    #
+    # Click on "Other services" button
+    time.sleep(round(2000 / 1000))
+    driver.find_element(By.CSS_SELECTOR, ".QflowObjectItem:nth-child(7)").click()
+
+    #
+    # Click on new drivers license button.
+    time.sleep(round(2000 / 1000))
+    driver.find_element(By.CSS_SELECTOR, ".QflowObjectItem:nth-child(4)").click()
+
+
+    #
+    # Click on the Arlington office.
+    time.sleep(round(2000 / 1000))
+    driver.find_element(By.CSS_SELECTOR, ".QflowObjectItem:nth-child(3)").click()
+
+
+    #
+    # Get the next available appointment.
+    time.sleep(round(2000 / 1000))
+    el = driver.find_element(By.CSS_SELECTOR, ".DateTimeGrid-Day:nth-child(1)")
+
+    #
+    # Convert the next available appointment to epoch
+    str = el.text.strip()
+    my_date = datetime.strptime(str, "%m/%d/%Y")
+    bestTimeEpoch = my_date.timestamp() * 1000
+
+    print("The soonest time is: ", el.text)
+
+    #
+    # Get the users time restraint, let them know if the next available appointment 
+    # is within the restraint.
+    user = userData[0]
+    userEpoch = user.get("timeLimit")
+    userNumber = "+" + user.get("number")
+
+
+    #
+    # Send a message to the user
+    message = "Hurry up, there is a DMV appointment available at " + str + "!!!"
+
+    print(userEpoch)
+    print(bestTimeEpoch)
+
+    if bestTimeEpoch <= userEpoch:
+        send_to = '+12408104360'
+        message = client.messages.create(
+            body = message,
+            from_ = twilio_number,
+            to = userNumber
+        )
+        driver.quit()
+        break
+
+
+    #
+    # Close the current window.
+    driver.quit()
